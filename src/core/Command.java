647feb8e4fc98e5cca145ea2630b36b431a44d31
haski <perepelkin2601@gmail.com>
2019-09-14 14:51:56 +0500

    1. Add class new Command

---
+++ src/core/Command.java
@@ -0,0 +1,23 @@
+package core;
+
+public class Command implements ICommand
+{
+    private String command;
+    private String[] params;
+
+    public Command(String command, String[] params)
+    {
+        this.command = command;
+        this.params = params;
+    }
+
+    public String command()
+    {
+        return command;
+    }
+
+    public String[] params()
+    {
+        return params;
+    }
+}
