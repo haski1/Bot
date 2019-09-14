b83cb1a146f437f987b6ef10e8c345d352906115
haski <perepelkin2601@gmail.com>
2019-09-14 14:38:13 +0500

    1. Add new interface ICommand

---
+++ src/core/ICommand.java
@@ -0,0 +1,6 @@
+package core;
+
+public interface ICommand {
+    String command();
+    String[] params();
+}
--- src/core/Main.java
+++ src/core/Main.java
@@ -4,7 +4,5 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Тест");
-        System.out.println("Тест №2");
     }
 }
