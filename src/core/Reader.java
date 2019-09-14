36d01c4dc8edf019b51d5f865f869ba737299015
haski <perepelkin2601@gmail.com>
2019-09-14 15:11:55 +0500

    1. Add new class Reader 2. Add new interface IHandler

--- src/core/Reader.java
+++ src/core/Reader.java
@@ -1,6 +1,34 @@
 package core;
 
+import java.util.Scanner;
+
 public class Reader
 {
+    private IHandler handler;
+    private IParser parser;
+    private Scanner scanner;
+    private boolean isRunning;
 
+    public Reader(IHandler handler, IParser parser)
+    {
+        this.handler = handler;
+        this.parser = parser;
+        scanner = new Scanner(System.in);
+    }
+
+    public void run()
+    {
+        isRunning = true;
+        while (isRunning)
+        {
+            String input = scanner.next();
+            ICommand cmd = parser.parse(input);
+            handler.handle(cmd);
+        }
+    }
+
+    public void stop()
+    {
+        isRunning = false;
+    }
 }
