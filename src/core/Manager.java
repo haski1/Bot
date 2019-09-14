19db48f9e70b415b8e5dd9bdc681d21bd90903c0
haski <perepelkin2601@gmail.com>
2019-09-14 18:21:16 +0500

    Functionality for Manager

--- src/core/Manager.java
+++ src/core/Manager.java
@@ -2,27 +2,50 @@ package core;
 
 import architecture.IManager;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 
 public class Manager implements IManager
 {
-    private FileReader reader;
+    private BufferedReader reader;
     private String context;
 
-    public Manager()
-    {
+    public Manager(){
+        File file = new File("questions.txt");
+        FileReader fileReader = null;
         try
         {
-
+            fileReader = new FileReader(file);
         }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        reader = new BufferedReader(fileReader);
     }
 
-    public String get() {
-        return null;
+    public String get()
+    {
+        StringBuilder output = new StringBuilder();
+        String line = new String();
+
+        while(!line.contains("Answer"))
+        {
+            output.append(line);
+            try
+            {
+                line = reader.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        context = line.split(" ")[1];
+        return output.toString();
     }
 
-    public boolean check(String answer) {
+    public boolean check(String answer)
+    {
         return false;
     }
 }
