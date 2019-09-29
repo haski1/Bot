5038d3d027a72d981c13f308562337c343eb697b
haski1 <47828732+haski1@users.noreply.github.com>
2019-09-29 20:30:07 +0500

    Del token

--- src/platforms/telegram/BotInfo.java
+++ src/platforms/telegram/BotInfo.java
@@ -3,5 +3,5 @@ package platforms.telegram;
 public class BotInfo
 {
     public static final String name = "iwanttoseebot";
-    public static final String token = "716917910:AAErDlnuFWFKvXEBZJgTi5fEEVgLKAnh9iY";
+    public static final String token = "";
 }
