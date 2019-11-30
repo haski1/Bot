8d97749f0f61392739752f56437096f519bb48c9
haski <perepelkin2601@gmail.com>
2019-11-30 15:57:44 +0500

    Uncomment

--- src/platforms/PlatformsSet.java
+++ src/platforms/PlatformsSet.java
@@ -14,16 +14,16 @@ public class PlatformsSet extends HashMap<Platform, IO>
     public PlatformsSet(IO handler)
     {
         super();
-        //try
-        //{
-        //    this.put(Platform.Telegram, new TelegramIO(handler, new File("resources/config.json")));
-        //   System.out.println("load platform Telegram - ok");
-        //}
-        //catch (IOException e)
-        //{
-        //    System.out.println("load platform Telegram - fail");
-        //    System.out.println(e.getMessage());
-        //}
+        try
+        {
+            this.put(Platform.Telegram, new TelegramIO(handler, new File("resources/config.json")));
+           System.out.println("load platform Telegram - ok");
+        }
+        catch (IOException e)
+        {
+            System.out.println("load platform Telegram - fail");
+            System.out.println(e.getMessage());
+        }
         try
         {
             this.put(Platform.Terminal, new TerminalIO(handler));
